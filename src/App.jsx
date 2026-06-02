a3edeb522578d1697f0fd375fabe13dd7e8d89f2
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2026-06-02 12:36:08 +0530

    ex 2.6 add name to phone book

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
-const App = () => <div>Hello</div>;
+import { useState } from "react";
+
+const App = () => {
+  const [phoneBook, setPhoneBook] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNewNameChange = (e) => setNewName(e.target.value);
+
+  const addNewName = (e) => {
+    e.preventDefault();
+    setPhoneBook(phoneBook.concat({ name: newName }));
+  };
+
+  return (
+    <div>
+      <h1>Phone Book</h1>
+      <form onSubmit={addNewName}>
+        <p>
+          name:
+          <input type="text" value={newName} onChange={handleNewNameChange} />
+        </p>
+        <button>Add</button>
+      </form>
+      <h1>Numbers</h1>
+      {phoneBook.map((detail) => (
+        <p key={detail.name}>{detail.name}</p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
