c3bbb2f02b53a9ddec024ede699a67f889c26aa3
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2026-06-02 12:53:23 +0530

    ex 2.8 add number field to show number

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-  const [phoneBook, setPhoneBook] = useState([{ name: "Arto Hellas" }]);
+  const [phoneBook, setPhoneBook] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
-
-  const handleNewNameChange = (e) => setNewName(e.target.value);
+  const [newNumber, setNewNumber] = useState("");
 
   const addNewName = (e) => {
     e.preventDefault();
     const existingNames = phoneBook.map((detail) => detail.name);
     existingNames.includes(newName)
       ? alert(`${newName} is already added to phonebook`)
-      : setPhoneBook(phoneBook.concat({ name: newName }));
+      : setPhoneBook(phoneBook.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -21,13 +23,27 @@ const App = () => {
       <form onSubmit={addNewName}>
         <p>
           name:
-          <input type="text" value={newName} onChange={handleNewNameChange} />
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </p>
+        <p>
+          Number:
+          <input
+            type="text"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </p>
         <button>Add</button>
       </form>
       <h1>Numbers</h1>
       {phoneBook.map((detail) => (
-        <p key={detail.name}>{detail.name}</p>
+        <p key={detail.name}>
+          {detail.name} {detail.number}
+        </p>
       ))}
     </div>
   );
