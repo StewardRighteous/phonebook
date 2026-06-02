b28396ab62d01a9f437c7dc9704116b1d49ce455
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2026-06-02 13:02:15 +0530

    ex 2.9 add search to search numbers

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [phoneBook, setPhoneBook] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const addNewName = (e) => {
     e.preventDefault();
@@ -17,9 +21,25 @@ const App = () => {
     setNewNumber("");
   };
 
+  const namesToShow =
+    search.trim() === ""
+      ? phoneBook
+      : phoneBook.filter((detail) =>
+          detail.name.toLowerCase().includes(search.toLowerCase()),
+        );
+
   return (
     <div>
       <h1>Phone Book</h1>
+      <p>
+        filter shown with{" "}
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </p>
+      <h2>Add a New</h2>
       <form onSubmit={addNewName}>
         <p>
           name:
@@ -39,8 +59,8 @@ const App = () => {
         </p>
         <button>Add</button>
       </form>
-      <h1>Numbers</h1>
-      {phoneBook.map((detail) => (
+      <h2>Numbers</h2>
+      {namesToShow.map((detail) => (
         <p key={detail.name}>
           {detail.name} {detail.number}
         </p>
